b4311391591ecb136ad12e048833c479df117d59
Rafael Lorandi <coolparadox@gmail.com>
2019-07-29 00:49:44 -0300

    (wip) types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+enum EgestionCode {
+    Amplify,
+    Uncover,
+}
+
+enum Primer {
+    Turn,
+    Reflect,
+    Ground,
+}
+
+enum Value {
+    Zero,
+    NonZero(Option<Primer>, Number),
+}
+
+struct Number {
+    strategy: Box<dyn Strategy>,
+}
+
+trait Strategy {
+    fn egest(&self) -> StrategyOutput;
+}
+
+enum StrategyOutput {
+    Egestion(Option<EgestionCode>),
+    Exhaustion(Box<dyn Strategy>),
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
