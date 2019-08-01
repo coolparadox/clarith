8e97ae39f12d50046a577fc95a127e4f9e872752
Rafael Lorandi <coolparadox@gmail.com>
2019-08-01 07:38:53 -0300

    Define public interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,25 @@
-mod protocol;
+pub mod protocol;
 mod strategy;
 
 use crate::strategy::Strategy;
 
-enum Value {
+pub enum Value {
     Zero,
-    NonZero(Option<protocol::Primer>, Number),
+    NegInf,
+    PosInf,
+    Other(Option<protocol::Primer>, Number),
 }
 
-struct Number {
+pub struct Number {
     strategy: Box<dyn Strategy>,
 }
 
-fn one() -> Value {
-    Value::NonZero(None, Number{strategy: Box::new(strategy::one::new())})
+pub fn one() -> Value {
+    Value::Other(None, Number{strategy: Box::new(strategy::one::new())})
 }
 
 impl Number {
-    fn egest(&mut self) -> Option<protocol::Reduction> {
+    pub fn egest(&mut self) -> Option<protocol::Reduction> {
         match self.strategy.egest() {
             Ok(message) => message,
             Err(new_strategy) => {
@@ -36,7 +38,7 @@ mod tests {
 
     #[test]
     fn test_one() {
-        if let Value::NonZero(None, mut number) = one() {
+        if let Value::Other(None, mut number) = one() {
             assert_eq!(number.egest(), None);
         }
         else {
