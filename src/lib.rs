5beda88556ce05d12f4c171d0e391a11a130b983
Rafael Lorandi <coolparadox@gmail.com>
2019-08-04 23:37:14 -0300

    Documentation on public entities
    TODO: crate introduction

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,38 @@
+/// Building elements of rational values.
 pub mod protocol;
 mod strategy;
 
 use crate::strategy::Strategy;
 
+/// A rational value greater than zero and lesser than one.
 pub struct Number {
     strategy: Box<dyn Strategy>,
 }
 
 impl Number {
+
+    /// Removes information from a Number instance.
+    /// Calling this method changes Number as a side effect.
+    /// Returns the next information component extracted from Number, or None if Number is one half.
     pub fn egest(&mut self) -> Option<protocol::Reduction> {
         match self.strategy.egest() {
-            Ok(message) => message,
+            Ok(reduction) => reduction,
             Err(new_strategy) => {
                 self.strategy = new_strategy;
                 self.egest()
             }
         }
     }
+
 }
 
+/// A rational value with unbounded range and precision.
 pub enum Value {
     Special(protocol::Fixed),
     Other(Option<protocol::Primer>, Number),
 }
 
+/// Construct a rational value from the ratio of two signed machine integers.
 pub fn ratio(num: isize, den:isize) -> Value {
     ratio_u(
         (num >= 0 && den >= 0) || (num < 0 && den < 0),
@@ -31,6 +40,7 @@ pub fn ratio(num: isize, den:isize) -> Value {
         if den >= 0 { den } else { -den } as usize)
 }
 
+/// Construct a rational value from the ratio of two unsigned machine integers.
 pub fn ratio_u(positive: bool, num: usize, den: usize) -> Value {
     let (special, primer, ratio) = strategy::ratio::new(positive, num, den);
     if let Some(fixed) = special {
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,21 +1,34 @@
+/// Basic components of a Number.
 #[derive(Debug, PartialEq)]
 pub enum Reduction {
+    /// Number was greater than zero and lesser than one half, and was doubled.
     Amplify,
+    /// Number was greater than one half and no greater than one, was reciprocated and then had one subtracted from itself.
     Uncover,
 }
 
+/// Ways to increase the representation range of a Number.
 #[derive(Debug, PartialEq)]
 pub enum Primer {
+    /// The original value was greater than one and was reciprocated. 
     Turn,
+	/// The original value was lesser than zero but no lesser than minus one, and was negated. 
     Reflect,
+	/// The original value was lesser than minus one, was negated and then was reciprocated.
     Ground,
 }
 
+/// Values that cannot be uniquely represented by the combination of a Primer and a Number.
 #[derive(Debug, PartialEq)]
 pub enum Fixed {
+	/// Negative infinity
     NegInf,
+	/// Minus one
     NegOne,
+	/// Zero
     Zero,
+	/// One
     One,
+	/// Positive infinity
     PosInf,
 }
