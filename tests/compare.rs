729fb1b714867b513053f152d88f84a0c9f939ff
Rafael Lorandi <coolparadox@gmail.com>
2019-09-09 09:13:01 -0300

    Run integration tests in parallel

--- tests/compare.rs
+++ tests/compare.rs
@@ -21,25 +21,6 @@
 use std::cmp::Ordering;
 use clarith::Number;
 
-fn reference_compare(mut n1: isize, mut d1: isize, mut n2: isize, mut d2: isize) -> Ordering {
-    assert!(n1 != 0 || d1 != 0);
-    assert!(n2 != 0 || d2 != 0);
-    if d1 == 0 { n1 /= n1.abs(); }
-    if d2 == 0 { n2 /= n2.abs(); }
-    if d1 < 0 { n1 *= -1; d1 *= -1; }
-    if d2 < 0 { n2 *= -1; d2 *= -1; }
-    let c = if d1 != 0 || d2 != 0 { n1 * d2 - n2 * d1 } else { n1 - n2 };
-    if c > 0 {
-        Ordering::Greater
-    }
-    else if c < 0 {
-        Ordering::Less
-    }
-    else {
-        Ordering::Equal
-    }
-}
-
 #[test]
 fn compare_ratios() {
     let range = 25;
@@ -62,4 +43,64 @@ fn compare_ratios() {
             }
         }
     }
+}
+
+#[test]
+fn compare_homographics() {
+    let range = 6;
+    for a in -range..range {
+        for b in -range..range {
+            for c in -range..range {
+                for d in -range..range {
+                    if a == 0 && b == 0 && c == 0 && d == 0 {
+                        continue;
+                    }
+                    for xn in -range..range {
+                        for xd in -range..range {
+                            if xn == 0 && xd == 0 {
+                                continue;
+                            }
+                            let (rn, rd) = result_ratio(a, b, c, d, xn, xd);
+                            if rn == 0 && rd == 0 {
+                                continue;
+                            }
+                            // println!("{} {} {} {} ({} {}) = {} {}", a, b, c, d, xn, xd, rn, rd);
+                            assert_eq!(Number::compare(Number::homographic(Number::ratio(xn, xd), a, b, c, d), Number::ratio(rn, rd)), Ordering::Equal);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn reference_compare(mut n1: isize, mut d1: isize, mut n2: isize, mut d2: isize) -> Ordering {
+    assert!(n1 != 0 || d1 != 0);
+    assert!(n2 != 0 || d2 != 0);
+    if d1 == 0 { n1 /= n1.abs(); }
+    if d2 == 0 { n2 /= n2.abs(); }
+    if d1 < 0 { n1 *= -1; d1 *= -1; }
+    if d2 < 0 { n2 *= -1; d2 *= -1; }
+    let c = if d1 != 0 || d2 != 0 { n1 * d2 - n2 * d1 } else { n1 - n2 };
+    if c > 0 {
+        Ordering::Greater
+    }
+    else if c < 0 {
+        Ordering::Less
+    }
+    else {
+        Ordering::Equal
+    }
+}
+
+fn result_ratio(a: isize, b: isize, c: isize, d: isize, mut xn: isize, mut xd: isize) -> (isize, isize) {
+    assert!(xn != 0 || xd != 0);
+    if xn == 0 {
+        xd = 1;
+    }
+    if xd < 0 {
+        xn *= -1;
+        xd *= -1;
+    }
+    (a * xn + b * xd, c * xn + d * xd)
 }
